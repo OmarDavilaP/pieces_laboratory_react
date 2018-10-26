fdf0fd4c5c2a0dade7e5522092157539c9dc5ddb
Omar Davila <morrison.x@gmail.com>
2018-10-25 21:26:56 -0500

    split it in small components

--- client/src/component/common/control-panel/control-panel.jsx
+++ client/src/component/common/control-panel/control-panel.jsx
@@ -3,44 +3,30 @@ import { connect } from "react-redux";
 //external libraries
 import Joi from "joi-browser";
 import Input from '../input/input';
+import AddButton from '../add-button/add-button'
 class ControlPanel extends Component {
-  state={
-    showAlertMessage:false,
-    message:''
+
+  state = {
+    showAlertMessage: false,
+    message: ''
   }
 
-  handleAlert=(message)=>{
-    this.setState({showAlertMessage:!!message,message});
+  handleAlert = (message) => {
+    this.setState({ showAlertMessage: !!message, message });
 
   }
 
-  renderAlert=()=><div className="alert alert-danger" role="alert" >{this.state.message}</div>
+  renderAlert = () => <div className="alert alert-danger" role="alert" >{this.state.message}</div>
 
-  renderInput=(tableStr)=>{
-   return tableStr.map((item, index,arr) => {
-      return ( 
-      <Input key={index} 
-       validate={item.schema} 
-       alert={item.alert} 
-       alertMessage={this.handleAlert }             
-      />)
+  renderInput = (tableStr) => {
+    return tableStr.map((item, index, arr) => {
+      return (
+        <Input key={index}
+          validate={item.schema}
+          alert={item.alert}
+          alertMessage={this.handleAlert}
+        />)
     })
-  }
-  
-
-  // shouldComponentUpdate(prev,post){
-  //   console.log("?",post)
-  //   return false;
-  // }
-  
-  renderAddButton(){
-    return (
-      <li className="nav-item">
-        <a className="nav-link active" href="#">
-          <i className="fa-shopping-cart" />
-        </a>
-      </li>     
-    );
   }
   render() {
     const { tableStr } = this.props;
@@ -49,9 +35,9 @@ class ControlPanel extends Component {
       <ul className="nav nav-pills control-panel">
         <div className="input-group mb-sm-0">
           {this.renderInput(tableStr)}
-          {this.renderAddButton()}
+          <AddButton />
         </div>
-        { this.state.showAlertMessage && this.renderAlert()}
+        {this.state.showAlertMessage && this.renderAlert()}
       </ul>
     );
   }
--- client/src/component/common/input/input.jsx
+++ client/src/component/common/input/input.jsx
@@ -4,31 +4,29 @@ import Joi from "joi-browser";
 
 class Input extends Component {
 
-  constructor(){
+  constructor() {
     super();
-    this.inputElement=React.createRef();
+    this.inputElement = React.createRef();
   }
 
   validateInput = (e, schema) => {
     const inputObj = e.target.value;
-    const {error,value} = Joi.validate({value:inputObj}, schema, { abortEarly: false });
-    let message=(!!error)?error.message:error;
+    const { error, value } = Joi.validate({ value: inputObj }, schema, { abortEarly: false });
+    let message = (!!error) ? error.message : error;
     this.props.alertMessage(message);
   };
-  // shouldComponentUpdate(prev,after){
-  //   if(after.errorValidation!=this.errorValidation) return true;
-  //   return false;
-  // }
+
   render() {
     const { validate, alert } = this.props;
+    console.log(validate)
     return (
-       <React.Fragment>
-          <input className="form-control"
-            type="text"
-            onChange={event => this.validateInput(event, validate)}
-            ref={this.inputElement}
-          />
-         </React.Fragment>
+      <React.Fragment>
+        <input className="form-control"
+          type="text"
+          onChange={event => this.validateInput(event, validate)}
+          ref={this.inputElement}
+        />
+      </React.Fragment>
     );
   }
 }
